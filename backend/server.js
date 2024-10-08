4b4084e68f1cebd9ae697f718c86df05e9d4f27d
Aasif Azlan <aasifazlaan@gmail.com>
2024-10-08 21:34:08 +0530

    signup login logout

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import { connectDB } from './lib/db.js';
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 const PORT= process.env.PORT
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB()
 });
+
+app.use('/api/v1/auth', authRoutes)
